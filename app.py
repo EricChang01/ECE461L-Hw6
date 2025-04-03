1601faca82b084e8c7cd9718c7691cdbd557e97a
Eric Chang <changyungta2001@gmail.com>
2025-04-03 17:43:19 -0500

    reorganize files

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
-app = Flask(__name__, static_folder='../client/build', static_url_path='')
+app = Flask(__name__, static_folder='./build', static_url_path='/')
 CORS(app)
 # Endpoint for checking in hardware
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
+
 @app.route('/checkInHardware', methods=['GET'])
 def check_in_hardware():
     project_id = request.args.get('projectId')
@@ -44,4 +53,4 @@ def leave_project():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
