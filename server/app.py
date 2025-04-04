0f5c3cdb120eb6e39ca3906340865214a96d66ec
Eric Chang <changyungta2001@gmail.com>
2025-04-03 20:00:53 -0500

    rollback to using GET

--- server/app.py
+++ server/app.py
@@ -13,49 +13,45 @@ def serve(path):
     else:
         return send_from_directory(app.static_folder, 'index.html')
 
-# Endpoint for checking in hardware (POST)
-@app.route('/api/checkInHardware', methods=['POST'])
+@app.route('/api/checkInHardware', methods=['GET'])
 def check_in_hardware():
-    data = request.get_json() or {}
-    project_id = data.get('projectId')
-    qty = data.get('qty')
+    project_id = request.args.get('projectId')
+    qty = request.args.get('qty')
     
     return jsonify({
         "projectId": project_id,
         "qty": qty,
     })
 
-# Endpoint for checking out hardware (POST)
-@app.route('/api/checkOutHardware', methods=['POST'])
+# Endpoint for checking out hardware
+@app.route('/api/checkOutHardware', methods=['GET'])
 def check_out_hardware():
-    data = request.get_json() or {}
-    project_id = data.get('projectId')
-    qty = data.get('qty')
+    project_id = request.args.get('projectid')
+    qty = request.args.get('qty')
     
     return jsonify({
         "projectId": project_id,
         "qty": qty,
     })
 
-# Endpoint for joining a project (POST)
-@app.route('/api/joinProject', methods=['POST'])
+# Endpoint for joining a project
+@app.route('/api/joinProject', methods=['GET'])
 def join_project():
-    data = request.get_json() or {}
-    project_id = data.get('projectId')
+    project_id = request.args.get('projectid')
     
     return jsonify({
         "projectId": project_id,
     })
 
-# Endpoint for leaving a project (POST)
-@app.route('/api/leaveProject', methods=['POST'])
+# Endpoint for leaving a project
+@app.route('/api/leaveProject', methods=['GET'])
 def leave_project():
-    data = request.get_json() or {}
-    project_id = data.get('projectId')
+    project_id = request.args.get('projectid')
 
     return jsonify({
         "projectId": project_id,
     })
 
 if __name__ == '__main__':
+    # app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
     app.run(debug=True)
